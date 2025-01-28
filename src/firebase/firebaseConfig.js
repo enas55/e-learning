746a54431ffc9d568c1df130e4ac4e8e043ecc94
enastaher55 <enastaher06@gmail.com>
2025-01-28 18:20:47 +0200

    project structure

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCqZPgD51VXfB9JPrQhGL6bYZo4smYgig4",
+    authDomain: "e-learning-83415.firebaseapp.com",
+    projectId: "e-learning-83415",
+    storageBucket: "e-learning-83415.firebasestorage.app",
+    messagingSenderId: "314653804228",
+    appId: "1:314653804228:web:316edc526c64a2ae59eb91"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
